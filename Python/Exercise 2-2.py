e43a9faf4705f913e34661a9a07a9aa7a4fcdee4
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2022-07-06 18:56:12 -0400

    Exercise - Altitude of a satellite's orbit

---
+++ Python/Exercise 2-2.py
@@ -0,0 +1,13 @@
+# Objective: Write a program that takes a user input for the period of
+# a satellite's orbit, and then outputs the necessary altitude of the
+# satellite.
+
+G = 6.67e-11 # Newton's gravitational constant
+M = 5.97e24 # mass of the earth
+R = 6371e3 # radius of the earth
+pi = 3.1415926535 # pi, rounded
+
+T = float(input("Enter the period of the satellite's orbit, in seconds: "))
+h = G*M*T**2 / 4*pi**2 - R # satellite's altitude
+
+print("The satellite must be ", h, "meters from the earth's surface.")
