b8d445c478eea1dcec4b018acd46bb6695adb1f1
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2022-07-07 11:09:14 -0400

    Update Exercise - Altitude of satellite's orbit.py

--- Python/Exercise - Altitude of satellite's orbit.py
+++ Python/Exercise - Altitude of satellite's orbit.py
@@ -8,8 +8,17 @@ R = 6371e3 # radius of the earth
 pi = 3.1415926535 # pi, rounded
 
 T = float(input("Enter the period of the satellite's orbit, in seconds: "))
-h = G*M*T**2 / 4*pi**2 - R # satellite's altitude
+h = (G*M*T**2 / (4*pi**2) )**(1/3) - R # satellite's altitude
 
 print("The satellite must be ", h, "meters from the earth's surface.")
 
 # The derivation for the formula used can be seen in "satellite altitude derivation.jpg".
+
+# If the program returns a negative output (i.e. in the case of a 45-minute
+# orbit), then this implies that Thrash would have to be below the earth's
+# surface. Therefore, the orbit is impossible.
+
+# For a satellite orbiting the earth once a day (24 hour period), the required
+# altitude is about 35855910.18 meters. For a sidereal day's orbit
+# (23.93 hours), the necessary altitude is about 35773762.33 meters.
+# That's a difference of about 82147.85 meters.
