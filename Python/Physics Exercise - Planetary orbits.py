f54337ad4666cc60b7e275aaf678bef0c3779d7c
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2022-07-15 22:28:47 -0400

    Add files via upload

---
+++ Python/Physics Exercise - Planetary orbits.py
@@ -0,0 +1,5 @@
+# Objective: Calculate various properties of an elliptical (solar) orbit, given
+# the speed and distance from the sun at the orbit's parihelion.
+
+l1 = float(input("Enter the shortest distance to the sun: "))
+v1 = float(input("Enter the planet's speed at that distance: "))
