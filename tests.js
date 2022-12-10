c1a977db08b204468a0c044a7fe94b665f05c33d
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2022-12-10 09:45:04 -0500

    Add files via upload

---
+++ tests.js
@@ -0,0 +1,5 @@
+// Identifies the third letter of the variable name.
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
