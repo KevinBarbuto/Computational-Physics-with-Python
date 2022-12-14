dc3d4ffb186388016d84cd5a5ee8228b3c3e9e9d
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2022-12-14 09:52:47 -0500

    Update site.php

--- Web development/PHP/site.php
+++ Web development/PHP/site.php
@@ -7,14 +7,16 @@
   <body>
   
     <form action="site.php" method="get">
-	  Name: <input type="text" name="username">
-	  <br>
-	  Age: <input type="text" name="age">
-	  <input type="submit">
-	</form>
-	<br>
-	Your name is <?php echo $_GET["username"] ?>
-	<br>
-	Your age is <?php echo $_GET["age"] ?>
+      Name: <br><input type="text" name="username">
+      <br>
+      Age: <br><input type="text" name="age">
+      <br><input type="submit">
+    </form>
+    <br>
+    <?php if( isset($_GET["username"]) && isset($_GET["age"]) ): ?>
+      Your name is <?php echo $_GET["username"] ?>
+      <br>
+      Your age is <?php echo $_GET["age"] ?>
+    <?php endif; ?>
   </body>
 </html>
