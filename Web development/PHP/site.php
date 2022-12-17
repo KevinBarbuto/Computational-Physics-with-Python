1ecdceeef1032936c8db37d956fc5a74142695c7
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2022-12-17 09:57:11 -0500

    Update site.php

--- Web development/PHP/site.php
+++ Web development/PHP/site.php
@@ -5,19 +5,14 @@
     <title>PHP Testing</title>
   </head>
   <body>
-      
-    <form action="site.php" method="post">
-      Apples: <input type="checkbox" name="fruits[]" value="apples"><br>
-      Oranges: <input type="checkbox" name="fruits[]" value="oranges"><br>
-      Pears: <input type="checkbox" name="fruits[]" value="pears"><br>
-      <input type="submit">
-    </form>
     
     <?php
-    
-      $fruits = $_POST["fruits"];
-      echo $fruits[1];
-    
+        function sayHi($name){
+            echo "Hello $name";
+        }
+        
+        sayHi("Kevin")
      ?>
+    
   </body>
 </html>
