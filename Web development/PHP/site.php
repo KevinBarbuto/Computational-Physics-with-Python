39bc1dae751baf74ea041592ed981a8cfb5d379c
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2022-12-20 14:30:11 -0500

    Update site.php

--- Web development/PHP/site.php
+++ Web development/PHP/site.php
@@ -7,12 +7,17 @@
   <body>
     
     <?php
-      function cube($num){
-          echo "Hello";
-          return;
+      $isMale = false;
+      $isTall = false;
+      if ($isMale && $isTall){
+          echo "You are a tall male";
+      } elseif($isMale && !$isTall){
+          echo "You are a short male";
+      } elseif(!$isMale && $isTall){
+          echo "You are not male but are tall";
+      } else {
+          echo "You are not male and not tall";
       }
-      
-      echo cube(4);
      ?>
     
   </body>
