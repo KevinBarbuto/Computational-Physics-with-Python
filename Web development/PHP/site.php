179a53dbe1dfd0e384a50af79f13615471b0bb2d
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2022-12-20 16:05:41 -0500

    Update site.php

--- Web development/PHP/site.php
+++ Web development/PHP/site.php
@@ -6,21 +6,30 @@
   </head>
   <body>
     
+    <form action="site.php" method="post">
+      First Num:<br> <input type="number" step="0.1" name="num1"><br>
+      OP:<br> <input type="text" name="op"><br>
+      Second Num:<br> <input type="number" step="0.1" name="num2"><br>
+      <input type="submit">
+    </form>
+    
     <?php
-      
-      function getMax($num1, $num2, $num3) {
-          
-          if ($num1 >= $num2 && $num1 >= $num3){
-              return $num1;
-          } elseif($num2 >= $num1 && $num2 >= $num3) {
-              return $num2;
-          } else {
-              return $num3;
-          }
-      }
-      
-      echo getMax(3000, 3000, 400);
-      
+    $num1 = $_POST["num1"];
+    $num2 = $_POST["num2"];
+    $op = $_POST["op"];
+    
+    if($op == "+"){
+        echo $num1 + $num2;
+    } elseif($op == "-"){
+        echo $num1 - $num2;
+    } elseif($op == "*"){
+        echo $num1 * $num2;
+    } elseif($op == "/"){
+        echo $num1 / $num2;
+    } else{
+        echo "Invalid operator";
+    }
+    
      ?>
     
   </body>
