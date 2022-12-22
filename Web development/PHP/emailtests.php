9cb3ee2be3e4fabdf5a034e67a7c40457cde1078
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2022-12-21 22:29:36 -0500

    Add files via upload

---
+++ Web development/PHP/emailtests.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Testing</title>
+  </head>
+  <body>
+    
+    <?php
+      
+      $email = "kevinb(ar)but:::o@gm//ail.com";
+      
+      echo "$email <br><br>";
+      
+      // Sanitize email
+      $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+      
+      echo "$email <br><br>";
+      
+      // Validate email
+      if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+          
+          echo "This is a valid email. Good job.";
+      } else {
+          echo "This is not a valid email.";
+      }
+      
+     ?>
+    
+  </body>
+</html>
