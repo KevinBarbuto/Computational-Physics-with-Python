c77f926e570e8b55e55855ac4d254009d6ee5bf2
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2022-12-22 12:47:19 -0500

    Update site.php

--- Web development/PHP/site.php
+++ Web development/PHP/site.php
@@ -6,11 +6,25 @@
   </head>
   <body>
     
-    <?php include "header.html"; ?>
-      
-      <p>Test website page.</p>
-      
-    <?php include "footer.html"; ?>
+    <?php
+    class Book {
+        var $title;
+        var $author;
+        var $pages;
+        
+        function __construct($aTitle, $aAuthor, $aPages){
+            $this->title = $aTitle;
+            $this->author = $aAuthor;
+            $this->pages = $aPages;
+        }
+    }
+    
+    $book1 = new Book("The Road", "Cormac McCarthy", 400);
+    $book2 = new Book("Moby Dick", "Herman Melville", 500);
+    
+    echo $book1->title;
+    
+     ?>
     
   </body>
 </html>
