e35d59c1eff64a20d2a17a0ba6fa813279ebd22e
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2022-12-22 00:47:30 -0500

    Update emailtests.php

--- Web development/PHP/emailtests.php
+++ Web development/PHP/emailtests.php
@@ -8,18 +8,16 @@
     
     <?php
       
+      // Improper email
       $email = "kevinb(ar)but:::o@gm//ail.com";
-      
       echo "$email <br><br>";
       
       // Sanitize email
       $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-      
       echo "$email <br><br>";
       
       // Validate email
       if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-          
           echo "This is a valid email. Good job.";
       } else {
           echo "This is not a valid email.";
