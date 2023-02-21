7bd9d2725ac50da2202e9dd22fb5c0b6568e3b2d
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2023-02-20 22:11:33 -0500

    Add files via upload

---
+++ ProbAndStats/variability.R
@@ -0,0 +1,11 @@
+x <- c(1.75, 2.66, 1.97, 2.17, 1.82)
+
+xsum <- sum(x)
+xm <- mean(x)
+xmed <- median(x)
+xsorted <- sort(x) # Sorts the list. Useful for finding modes.
+xsd <- sd(x)
+xrange <- range(x)
+xvar <- var(x)
+
+print(2.66 - 1.75)
