638884087c55f84201f18df87cd22c1ad4037305
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2024-11-16 21:36:40 -0500

    Create numbers.c

---
+++ c/numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num = 6;
+
+    printf("%f\n", 8.9);
+    printf("%f\n", 5 + 4.5);
+    printf("%d\n", 5 / 4);
+    printf("%f\n", 5 / 4.0);
+
+    printf("%d\n", num);
+    printf("%f\n", pow(4, 3) );
+    printf("%f\n", sqrt(36) );
+    printf("%f\n", ceil(36.356) );
+    printf("%f\n", floor(36.356) );
+
+
+    return 0;
+}
